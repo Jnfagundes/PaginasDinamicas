73e6daa84aac512e76f5c2765c8db83beae80f5b
José Nilton <71674292+Jnfagundes@users.noreply.github.com>
2025-02-06 16:51:47 -0300

    Somando dois valores

--- logica.js
+++ logica.js
@@ -1,7 +1,17 @@
-var numero = 0;
 
-let numero2 = 0;
 
-const nome = 'João';
+//Somando dois valores
 
-console.log(numero);
+var num ;
+var num2;
+
+Number.num = prompt('Digite um número:');
+Number.num2 = prompt('Digite outro número:');
+
+console.log(typeof(num))
+console.log(typeof(num2))
+
+//let soma = 0
+//Number.soma = num + num2
+alert('A soma dos números é: '+(Number.num + Number.num2));
+
