e173a794e786c0171fa4459ae3a9ca4cc6f88293
José Nilton <71674292+Jnfagundes@users.noreply.github.com>
2025-02-07 16:24:29 -0300

    ok

--- logica.js
+++ logica.js
@@ -2,16 +2,15 @@
 
 //Somando dois valores
 
-let num ;
-let num2;
+let num = 0;
+let num2 = 0;
 
 num = Number(prompt('Digite um número:'));
 num2 = Number(prompt('Digite outro número:'));
 
-console.log(typeof(num))
-console.log(typeof(num2))
+//console.log(typeof(num))
+//console.log(typeof(num2))
 
-//let soma = 0
-//Number.soma = num + num2
-alert('A soma dos números é: '+(Number.num + Number.num2));
+soma = num + num2
+alert('A soma dos números é: '+ soma);
 
